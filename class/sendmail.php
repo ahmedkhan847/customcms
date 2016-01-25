948e30ee039f9562c4795fc4d849ebf5a4fe3944
Ahmed KHan <ahmedkhan_847@hotmail.com>
2016-01-25 13:28:58 +0500

    Email is done

--- class/sendmail.php
+++ class/sendmail.php
@@ -20,8 +20,8 @@ class SendMail
 		$mail->isSMTP();                                      // Set mailer to use SMTP
 		$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 		$mail->SMTPAuth = true;                               // Enable SMTP authentication
-		$mail->Username = 'ahmedkhangaditek@gmail.com';                 // SMTP username
-		$mail->Password = 'ahmedkhan92';                           // SMTP password
+		$mail->Username = 'example@gmail.com';                 // SMTP username
+		$mail->Password = 'secret';                           // SMTP password
 		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 		$mail->Port = 587;                                    // TCP port to connect to
 
