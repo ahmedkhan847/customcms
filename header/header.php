07ca62ca19799134692b1f668459cdce051b05da
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-05-17 16:28:08 +0500

    Update header.php

--- header/header.php
+++ header/header.php
@@ -13,8 +13,8 @@ and open the template in the editor.
          <link rel="amphtml" href="<?php echo $_GET['aid'];?>">
         <title>Custom Content Management System</title>
         <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="/customcms/style/css/bootstrap.min.css">
-        <link href="/customcms/style/css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="style/css/bootstrap.min.css">
+        <link href="style/css/style.css" rel="stylesheet">
         
         
     </head>
@@ -31,10 +31,10 @@ and open the template in the editor.
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="/customcms/">Home</a></li>
-        <li><a href="/customcms/contactus">Contact Us</a></li>
+        <li class="active"><a href="/">Home</a></li>
+        <li><a href="contactus">Contact Us</a></li>
       </ul>
-      <form class="navbar-form navbar-right" role="search" method="get" action="/customcms/search.php">
+      <form class="navbar-form navbar-right" role="search" method="get" action="search.php">
         <div class="form-group">
           <input type="text" class="form-control" name="q" placeholder="Search">
         </div>
@@ -45,16 +45,16 @@ and open the template in the editor.
         {
      ?>
       <ul class="nav navbar-nav navbar-right"> 
-      <li><a href="/customcms/dashboard"><span class="glyphicon glyphicon-user"></span> Dashboard</a></li>
-        <li><a href="/customcms/logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+      <li><a href="dashboard"><span class="glyphicon glyphicon-user"></span> Dashboard</a></li>
+        <li><a href="logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
       </ul>
       <?php 
         }
         else
         {?>
             <ul class="nav navbar-nav navbar-right"> 
-            <li><a href="/customcms/userreg"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-            <li><a href="/customcms/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a href="userreg"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
             </ul>
    <?php     }
 
